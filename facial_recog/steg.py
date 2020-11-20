165d01c6ab75479f13dc79aa91ca42fb0547ec28
ethan <ethanholland@outlook.com>
2020-11-19 19:57:39 -0600

    Remove reference to orginal_dataset in decode to help make one way encryption/decryption

--- facial_recog/steg.py
+++ facial_recog/steg.py
@@ -35,12 +35,12 @@ def menuDecode():
     imgPath = os.path.join(os.getcwd(), "facial_recog", "dataset", picture)
     toGetPoints = os.path.join(os.getcwd(), "facial_recog", "original_dataset", picture)
     facialFeature = 'nose'
-    pointsList = facial_features.do_facial_feature_recog(picture, toGetPoints, 1, facialFeature)
+    pointsList = facial_features.do_facial_feature_recog(picture, imgPath, 1, facialFeature)
     pointsList = pointsList[1]
     try:
         print("Decoded: {}".format(LSB.decode(picture,imgPath, pointsList)))
         #shutil will copy the original file and replace the encoded image with the original copy of the image
-        shutil.copyfile(toGetPoints, imgPath)  
+        # shutil.copyfile(toGetPoints, imgPath)  
     except StopIteration:
         print("Exception")
         shutil.copyfile(toGetPoints, imgPath)
